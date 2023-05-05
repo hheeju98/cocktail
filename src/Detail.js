8702ab486385e2bf3cfebddcdf930c599bf263bf
hheeju98 <hheeju98@naver.com>
2023-05-05 22:37:03 +0900

    상세보기 기능 완료

--- src/Detail.js
+++ src/Detail.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
 import DetailList from "./DetailList";
+import { useParams } from "react-router-dom";
 
-export default function Detail() {
+export default function Detail({ drinks }) {
+  const { idDrink } = useParams();
   const [data, setData] = useState([]);
   const getDetail = async () => {
     const json = await (
       await fetch(
-        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=17222`
+        `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idDrink}`
       )
     ).json();
     setData(json.drinks);
