972982745102b8b94e810ecd049fa01c6e3d933e
hheeju98 <hheeju98@naver.com>
2023-05-06 17:46:11 +0900

    로딩중 구현 완료

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,36 @@ import { useState, useEffect } from "react";
 import CocktailList from "./CocktailList";
 import "./CocktailList.css";
 export default function Home() {
+  const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const getCocktail = async () => {
     const json = await (
       await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a`)
     ).json();
     setData(json.drinks);
-    console.log(json.drinks);
+    setLoading(false);
   };
   useEffect(() => {
     getCocktail();
   }, []);
 
   return (
-    <div className="cocktailList">
-      {data.map((m) => (
-        <CocktailList
-          key={m.idDrink}
-          strDrinkThumb={m.strDrinkThumb}
-          strInstructions={m.strInstructions}
-          idDrink={m.idDrink}
-          strDrink={m.strDrink}
-        />
-      ))}
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="cocktailList">
+          {data.map((m) => (
+            <CocktailList
+              key={m.idDrink}
+              strDrinkThumb={m.strDrinkThumb}
+              strInstructions={m.strInstructions}
+              idDrink={m.idDrink}
+              strDrink={m.strDrink}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
